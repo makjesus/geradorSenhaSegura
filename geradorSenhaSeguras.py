7c1ef8973a6ca310aa011d5c466c7b3f657a3081
unknown <makjesus2008@gmail.com>
2025-09-02 15:22:41 -0300

    Realizando o commit para o arquivo local e remoto

---
+++ geradorSenhaSeguras.py
@@ -0,0 +1,18 @@
+from_future_import annotations 
+
+import argparse
+import math
+import secrets
+import string 
+import sys
+
+
+from typing import list 
+
+lower = "abcdefghijkmnopqrstuvwxyz" 
+upper = "ABCDEFGHJKLMNPQRSTUVWXYZ" 
+digits = "23456789"     
+symbols = "!@#$%^&*+-_=~?"  
+
+
+
